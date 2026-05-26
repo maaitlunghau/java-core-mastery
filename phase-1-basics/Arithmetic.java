28744c9df83e76e5289fff326e406ead2de96976
maaitlunghau <chunhau.py@gmail.com>
2026-05-26 13:26:15 +0700

    feat: implement arithmetic file

---
+++ phase-1-basics/Arithmetic.java
@@ -0,0 +1,31 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+
+        // Arithmetic Operators
+        int x = 10;
+        int y = 3;
+        int z;
+
+        // z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x / y;
+        // z = x % y;
+
+        // x += y;
+        // x -= y;
+        // x *= y;
+        // x /= y;
+        // x %= y;
+
+        // Increment and Decrement Operators
+        int a = 1;
+
+        // a++;
+        // a--;
+
+        // Order Of Operations [P-E-M-D-A-S]
+        double result = 3 + 4 * (7 - 5) / 2.0;
+        System.out.println("Result: " + result); // 7.0
+    }
+}
